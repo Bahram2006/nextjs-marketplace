fa64a7515bdd0a32b9cc6210b72c49868e2cd663
Bahram2006 <yardam034@gmail.com>
2026-04-16 09:00:59 +0500

    and hero section to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
 export default function Home() {
   return (
-    <div className="text-3xl font-bold mt-10">
-      Home Page 🚀
+    <div className="py-16 text-center">
+      <h1 className="text-5xl font-bold mb-4">
+        Find Anything You Want 🚀
+      </h1>
+
+      <p className="text-gray-500 text-lg mb-8">
+        Buy and sell products easily across Turkmenistan
+      </p>
+
+      <button className="bg-black text-white px-6 py-3 rounded-full hover:bg-gray-800 transition">
+        Start Exploring
+      </button>
     </div>
   );
 }
