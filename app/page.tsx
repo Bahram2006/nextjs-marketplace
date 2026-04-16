e8135d3a8bb93bf4925439239318363cab5c39a3
Bahram2006 <yardam034@gmail.com>
2026-04-16 13:30:40 +0500

    implement real-time search filtering

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,21 @@
 "use client";
 
 import { useState } from "react";
+import Navbar from "@/components/layout/Navbar";
 import ListingGrid from "@/features/listing/components/ListingGrid";
 import { listings as staticListings } from "@/features/listing/data/listing.data";
 
 export default function Home() {
-  const [listings] = useState(() => {
-    if (typeof window === "undefined") return staticListings;
+  const [search, setSearch] = useState("");
 
-    const saved = localStorage.getItem("listings");
-
-    if (saved) {
-      return [...JSON.parse(saved), ...staticListings];
-    }
-
-    return staticListings;
-  });
+  const filteredListings = staticListings.filter((item) =>
+    item.title.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <div>
+      <Navbar onSearch={setSearch} />
+
       <div className="py-16 text-center">
         <h1 className="text-5xl font-bold mb-4">
           Find Anything You Want 🚀
@@ -29,7 +26,7 @@ export default function Home() {
         </p>
       </div>
 
-      <ListingGrid listings={listings} />
+      <ListingGrid listings={filteredListings} />
     </div>
   );
 }
