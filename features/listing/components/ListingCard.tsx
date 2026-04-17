9a2e0580512c62c900b24d5ec4389a76964921c8
Bahram2006 <yardam034@gmail.com>
2026-04-17 10:51:50 +0500

    fix favorite state initialization without useEffect

--- features/listing/components/ListingCard.tsx
+++ features/listing/components/ListingCard.tsx
@@ -1,23 +1,55 @@
+"use client";
+
 import { Listing } from "../types/listing.types";
 import Link from "next/link";
+import { Heart } from "lucide-react";
+import { useState } from "react";
+import { getFavorites, toggleFavorite } from "@/lib/storage";
 
 type Props = {
   listing: Listing;
 };
 
 export default function ListingCard({ listing }: Props) {
+  const [isFav, setIsFav] = useState(() => {
+    if (typeof window === "undefined") return false;
+    const favs = getFavorites();
+    return favs.includes(listing.id);
+  });
+
   return (
     <Link href={`/listing/${listing.id}`}>
       <div className="bg-white rounded-2xl overflow-hidden shadow-sm hover:shadow-xl transition duration-300 cursor-pointer group">
         
-        <div className="overflow-hidden">
+        {/* IMAGE + FAVORITE */}
+        <div className="relative overflow-hidden">
           <img
             src={listing.images[0]}
             alt={listing.title}
             className="w-full h-48 object-cover group-hover:scale-110 transition duration-300"
           />
+
+          {/* ❤️ FAVORITE */}
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              toggleFavorite(listing.id);
+              setIsFav((prev) => !prev);
+            }}
+            className="absolute top-2 right-2 bg-white p-2 rounded-full shadow hover:scale-110 transition"
+          >
+            <Heart
+              size={18}
+              className={
+                isFav
+                  ? "fill-black text-black"
+                  : "text-gray-400"
+              }
+            />
+          </button>
         </div>
 
+        {/* INFO */}
         <div className="p-4">
           <h2 className="text-lg font-semibold">
             {listing.title}
